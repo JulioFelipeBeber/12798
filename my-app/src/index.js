a439126aeb5ee1b93fcfb4caea94d13df354fe39
JulioFelipeBeber <juliofelipebeber9912@gmail.com>
2022-08-24 18:13:07 -0300

    003

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,39 +6,100 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-class Relogio extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={date: new Date()};
+
+// function Ola(props) {
+//   return <h1>{props.name} - {props.lastName}</h1>
+// }
+
+// const Ola = (props) => {
+//   return <h1>{props.name} - {props.lastName}</h1>
+// }
+
+class Ola extends React.Component {
+  render() {
+    return <h1>{this.props.name} - {this.props.lastName} - {this.props.idade}</h1>
+  }
+}
+
+let dadosBasicos1 = {
+  nome: "Nome 01",
+  sobrenome: "Teste 001",
+  idade: 10
+}
+let dadosBasicos2 = {
+  nome: "Nome 02",
+  sobrenome: "Teste 002",
+  idade: 20
+}
+let dadosBasicos3 = {
+  nome: "Nome 03",
+  sobrenome: "Teste 003",
+  idade: 30
+}
+
+let enderecoResidencial1 = {
+  rua: "Rua residencial 1"
+}
+
+let enderecoComercial1 = {
+  rua: "Rua comercial 1"
+}
+
+class DadosBasicos extends React.Component {
+  render() {
+
+    return <h2>Dados Básicos -
+      {this.props.dados.nome} -
+      {this.props.dados.sobrenome} -
+      {this.props.dados.idade}</h2>
+  }
+}
+
+class Endereco extends React.Component {
+  render() {
+    return <h3>{this.tipo} - {this.props.endereco.rua}</h3>
   }
 
-componentDidMount() {
-  this.timerID= setInterval(() => this.tick(),1000);
-
 }
 
-tick() {
-  this.setState({date: new Date()})
+class DadosProfissionais extends React.Component {
+  render() {
+    return <h3>{this.props.ocupacao}</h3>
+  }
 }
 
-
-componentWillUnmont() {
-  clearInterval(this.timerID)
-}
-
-render() {
-  return (
-    <div>
-      <h1>Opa!!</h1>
-      <h2>Hora agora: {this.state.date.toLocaleTimeString()}</h2>
-    </div>
-  )
-}
-}
+// class Cadastro extends React.Component {
+//   render() {
+//     return <DadosBasicos dados={dadosBasicos}></DadosBasicos>
+//   }
+// }
 
 root.render(
-<Relogio />
+  <>
+    <h1>Cadastro Simples</h1>
+    <DadosBasicos dados={dadosBasicos1}></DadosBasicos>
+    <Endereco tipo="Residencial" endereco={enderecoResidencial1}></Endereco>
+    <Endereco tipo="Comercial" endereco={enderecoComercial1}></Endereco>
+    <DadosProfissionais ocupacao="Bartender"></DadosProfissionais>
+    <DadosProfissionais ocupacao="Programador"></DadosProfissionais>
+    <DadosProfissionais ocupacao="Atendente"></DadosProfissionais>
+
+    <DadosBasicos dados={dadosBasicos2}></DadosBasicos>
+    <Endereco tipo="Residencial" endereco={enderecoResidencial1}></Endereco>
+    <Endereco tipo="Comercial" endereco={enderecoComercial1}></Endereco>
+    <DadosProfissionais ocupacao="Bartender"></DadosProfissionais>
+    <DadosProfissionais ocupacao="Programador"></DadosProfissionais>
+    <DadosProfissionais ocupacao="Atendente"></DadosProfissionais>
+
+    <DadosBasicos dados={dadosBasicos3}></DadosBasicos>
+    <Endereco tipo="Residencial" endereco={enderecoResidencial1}></Endereco>
+    <Endereco tipo="Comercial" endereco={enderecoComercial1}></Endereco>
+    <DadosProfissionais ocupacao="Bartender"></DadosProfissionais>
+    <DadosProfissionais ocupacao="Programador"></DadosProfissionais>
+    <DadosProfissionais ocupacao="Atendente"></DadosProfissionais>
+  </>
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
